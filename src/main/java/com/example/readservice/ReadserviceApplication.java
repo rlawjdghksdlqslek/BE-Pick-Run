3dc21d8262b282df1b19e3f5d9f70e2d8f83ab2c
JeongHwan Kim <silverice1973@naver.com>
2025-05-31 18:16:49 +0900

    chore : init settings

---
+++ src/main/java/com/example/readservice/ReadserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.readservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/readservice/ReadserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.readservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
