6f1a946f5e871d7b5620e3522836a2cce316a667
rlawjdghksdlqslek <silverice1973@naver.com>
2025-06-05 16:27:29 +0900

    feat : Post Read 조회 API

--- src/main/java/com/example/readservice/application/PostReadService.java
+++ src/main/java/com/example/readservice/application/PostReadService.java
@@ -1,8 +1,11 @@
 package com.example.readservice.application;
 
+import com.example.readservice.dto.out.PostReadModelResDto;
 import com.example.readservice.kafka.event.PostCreatedEvent;
 
-public interface PostReadService{
+public interface PostReadService {
     void createPostRead(PostCreatedEvent postCreatedEvent);
+
+    PostReadModelResDto getPostRead(String postUuid);
 
 }
--- src/main/java/com/example/readservice/infrastructure/PostReadRepository.java
+++ src/main/java/com/example/readservice/infrastructure/PostReadRepository.java
@@ -3,8 +3,7 @@ package com.example.readservice.infrastructure;
 import com.example.readservice.entity.PostReadModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.Optional;
 
 public interface PostReadRepository extends MongoRepository<PostReadModel,String> {
-    Optional<PostReadModel> findByPostUuid(String id);
+    PostReadModel findByPostUuid(String id);
 }
