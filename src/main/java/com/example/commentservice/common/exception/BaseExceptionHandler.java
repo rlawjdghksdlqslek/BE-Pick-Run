9c1fd3586a35450ceed3aba18b335759b95ba279
rlawjdghksdlqslek <silverice1973@naver.com>
2025-06-09 19:49:07 +0900

    chore : comment-service 초기 세팅

---
+++ src/main/java/com/example/commentservice/common/exception/BaseExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.example.commentservice.common.exception;
+
+import com.example.commentservice.common.entity.BaseResponseEntity;
+import com.example.commentservice.common.response.BaseResponseStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class BaseExceptionHandler {
+
+    /**
+     * 발생한 예외 처리
+     */
+
+    @ExceptionHandler(BaseException.class)
+    protected ResponseEntity<BaseResponseEntity<Void>> BaseError(BaseException e) {
+        BaseResponseEntity<Void> response = new BaseResponseEntity<>(e.getStatus());
+        log.error("BaseException -> {}({})", e.getStatus(), e.getStatus().getMessage(), e);
+        return new ResponseEntity<>(response, response.httpStatus());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    protected ResponseEntity<BaseResponseEntity<Void>> RuntimeError(RuntimeException e) {
+        BaseResponseEntity<Void> response = new BaseResponseEntity<>(BaseResponseStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        log.error("RuntimeException: ", e);
+        for (StackTraceElement s : e.getStackTrace()) {
+            System.out.println(s);
+        }
+        return new ResponseEntity<>(response, response.httpStatus());
+    }
+
+}
