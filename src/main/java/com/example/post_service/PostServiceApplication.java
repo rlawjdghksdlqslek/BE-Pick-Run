e608ffddeaec2564fb43e3ecbcad42b6d7356fb7
rlawjdghksdlqslek <silverice1973@naver.com>
2025-06-09 19:34:04 +0900

    chore : EnableDiscoveryClient 어노테이션 추가

--- src/main/java/com/example/post_service/PostServiceApplication.java
+++ src/main/java/com/example/post_service/PostServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.post_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableJpaAuditing
 public class PostServiceApplication {
