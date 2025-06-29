e0021314d15670f42c203f2a5843a1f5168214e0
JeongHwan Kim <silverice1973@naver.com>
2025-06-29 14:11:24 +0900

    chore : first commit

---
+++ src/test/java/example/openvidu/OpenviduApplicationTests.java
@@ -0,0 +1,13 @@
+package example.openvidu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenviduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
