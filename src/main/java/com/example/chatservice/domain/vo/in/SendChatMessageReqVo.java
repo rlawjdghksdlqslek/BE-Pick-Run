a5e7961ad5c238d9315aa17d6538de839db46bcb
rlawjdghksdlqslek <silverice1973@naver.com>
2025-06-30 11:42:36 +0900

    chore : senderUuid 제거

--- src/main/java/com/example/chatservice/domain/vo/in/SendChatMessageReqVo.java
+++ src/main/java/com/example/chatservice/domain/vo/in/SendChatMessageReqVo.java
@@ -8,14 +8,12 @@ import lombok.Getter;
 public class SendChatMessageReqVo {
 
     private String chatRoomUuid;
-    private String senderUuid;
     private String receiverUuid;
     private String content;
 
     @Builder
-    public SendChatMessageReqVo(String chatRoomUuid, String senderUuid, String receiverUuid, String content) {
+    public SendChatMessageReqVo(String chatRoomUuid, String receiverUuid, String content) {
         this.chatRoomUuid = chatRoomUuid;
-        this.senderUuid = senderUuid;
         this.receiverUuid = receiverUuid;
         this.content = content;
     }
